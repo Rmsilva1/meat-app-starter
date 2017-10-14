d126893d585716181ee9de707295918c0d269bf6
Rafael Mateus <Rafael.Mateus.Silva@hotmail.com>
2017-10-14 14:55:38 -0300

    'detail'

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from '../restaurants/restaurants.service';
+import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class RestaurantDetailComponent implements OnInit {
+  
+  restaurant: Restaurant;
 
-  constructor() { }
+  constructor(private restaurantsService: RestaurantsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.restaurantsService.restaurantById(this.route.snapshot.params['id']).subscribe(restaurant => this.restaurant = restaurant);
+
   }
 
 }
